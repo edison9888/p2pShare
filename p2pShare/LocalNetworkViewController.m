bb21cfdce967234845616af73ef37cd23acb2cbc
jacobjiangwei <kof2003@126.com>
2013-07-15 23:08:45 +0800

    really find each other

--- p2pShare/LocalNetworkViewController.m
+++ p2pShare/LocalNetworkViewController.m
@@ -12,7 +12,8 @@
 {
     NSNetService            *_service;
     NSNetServiceBrowser     *_domainBrowser;
-    
+    NSNetService            *_tmpService;
+    NSMutableArray          *_services;
     // Keeps track of available domains
     NSMutableArray *_domains;
     
@@ -39,7 +40,7 @@
     self.view.backgroundColor=[UIColor grayColor];
     _domainBrowser = [[NSNetServiceBrowser alloc] init];
     [_domainBrowser setDelegate:self];
-    [_domainBrowser searchForRegistrationDomains];
+    [_domainBrowser searchForServicesOfType:@"_http._tcp" inDomain:@""];
     _domains = [[NSMutableArray alloc] init];
     _searching = NO;
 
@@ -80,6 +81,14 @@
     [self handleError:[errorDict objectForKey:NSNetServicesErrorCode]];
 }
 
+- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser didFindService:(NSNetService *)netService moreComing:(BOOL)moreServicesComing
+{
+    NSLog(@"name:%@ port:%d type:%@",[netService name],[netService port],[netService type]);
+    _tmpService=netService;
+    _tmpService.delegate=self;
+    [_tmpService resolveWithTimeout:5.0];
+}
+
 // Sent when a domain appears
 - (void)netServiceBrowser:(NSNetServiceBrowser *)browser
             didFindDomain:(NSString *)domainString
@@ -104,6 +113,46 @@
     {
         [self updateUI];
     }
+}
+
+// resolve nsnetservice
+
+// Sent when addresses are resolved
+
+// Verifies [netService addresses]
+- (BOOL)addressesComplete:(NSArray *)addresses
+           forServiceType:(NSString *)serviceType
+{
+    // Perform appropriate logic to ensure that [netService addresses]
+    // contains the appropriate information to connect to the service
+    return YES;
+}
+
+// Error handling code
+- (void)handleError:(NSNumber *)error withService:(NSNetService *)service
+{
+    NSLog(@"An error occurred with service %@.%@.%@, error code = %d",
+          [service name], [service type], [service domain], [error intValue]);
+    // Handle error here
+}
+
+- (void)netServiceDidResolveAddress:(NSNetService *)netService
+{
+    // Make sure [netService addresses] contains the
+    // necessary connection information
+    if ([self addressesComplete:[netService addresses]
+                 forServiceType:[netService type]]) {
+        [_services addObject:netService];
+        NSLog(@"resolved name:%@ port:%d type:%@",[netService name],[netService port],[netService type]);
+    }
+}
+
+// Sent if resolution fails
+- (void)netService:(NSNetService *)netService
+     didNotResolve:(NSDictionary *)errorDict
+{
+    [self handleError:[errorDict objectForKey:NSNetServicesErrorCode] withService:netService];
+    [_services removeObject:netService];
 }
 
 // Error handling code
